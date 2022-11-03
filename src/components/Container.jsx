8d9478490b04b380504449c55f1a662980cdb96c
Ömer NAYIR <omer.nayir.mdbf17@iste.edu.tr>
2022-11-03 15:19:26 +0300

    Header Weather file added

---
+++ src/components/Container.jsx
@@ -0,0 +1,14 @@
+import '../App.css'
+import Header from './Header'
+import Weather from './Weather'
+
+const Container = () => {
+    return (
+        <div className='Box'>
+            <Header />
+            <Weather />
+        </div>
+    )
+}
+
+export default Container
