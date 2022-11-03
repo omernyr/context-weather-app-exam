ac12095113252891c236edf457a6a8d05d9a611d
Ömer NAYIR <omer.nayir.mdbf17@iste.edu.tr>
2022-11-03 17:19:43 +0300

    api call fixes

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from "react"
+
 
 const Weather = () => {
-  return (
-    <div>Weather</div>
-  )
+
+    const [cityName, setCityName] = useState("turkey");
+    const myApiKey = 'c638a0809d1431a1185a33c20bb50304';
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myApiKey}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+
+    return (
+        <div className='Weather'>
+            <div className='daily-weather-box'>
+                <span className='weather-days'>Wed</span>
+                <span className='weather-icon'>☔️</span>
+                <span className='weather-deg'>78° 67°</span>
+            </div>
+            
+        </div>
+    )
 }
 
 export default Weather
