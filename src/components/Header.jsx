897309dd0f6aa2e44830ad08888bd0a195917db4
Ömer NAYIR <omer.nayir.mdbf17@iste.edu.tr>
2022-11-05 11:44:42 +0300

    country names added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 
 const Header = ({ searchCity, setSearchCity }) => {
 
+    const countrys = fetch("https://restcountries.com/v3.1/all").then((resp) => resp.json()).then((data) => console.log(data))
 
+    useEffect(() => {
+        console.log("Countries",countrys);
+    },[countrys])
+    
     return (
         <div className="Header">
             <form>
@@ -9,7 +15,10 @@ const Header = ({ searchCity, setSearchCity }) => {
                     value={searchCity}
                     onChange={(e) => setSearchCity(e.target.value)}
                     placeholder="Please a country name..." />
-
+                <select>
+                    
+                    <option value="turkey">turkey</option>
+                </select>
                 <button
                     type="submit">
                     Submit
