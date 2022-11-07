1541a832aeac72003e269196a9c57504079dd767
Ömer NAYIR <omer.nayir.mdbf17@iste.edu.tr>
2022-11-07 11:38:20 +0300

    some fixes

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,31 +1,34 @@
-import React from "react";
 import { useCountry } from "../context/CountryContext";
-
+import axios from "axios";
+import { useEffect } from "react";
 const Weather = () => {
 
-    const { cityDesc, setCityDesc, searchCity, submitVisible } = useCountry();
+    const { cityDesc, setCityDesc, searchCity, submitVisible, dailyForecast, setDailyForecast } = useCountry();
 
     const myApiKey2 = 'cd103e9bb5bb41dc840b6d7c45491b8d';
 
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchCity}&key=${myApiKey2}`
 
-    submitVisible && fetch(`${url}`)
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data);
-            // setCityDesc({
-            //     name: data.name,
-            //     temp: data.main.temp - 273.15
-            // })
+    submitVisible && axios.get(`${url}`)
+        .then((item) => {
+            item.data.data.map((day) =>
+                console.log(day)
+                // setCityDesc([...cityDesc, { name: searchCity, datetime: day.datetime, temp: day.temp, icon: day.weather.icon }])
+            )
+            // setDailyForecast([...dailyForecast, item.data])
         })
 
-    return (
+    // useEffect(() => {
+    //     console.log(cityDesc);
+    // }, [cityDesc])
 
+    return (
         <div className='Weather'>
             <div className='daily-weather-box'>
+
                 <span className='weather-days'>{cityDesc.name}</span>
                 <span className='weather-icon'>☔️</span>
-                <span className='weather-deg'>{cityDesc.temp.toFixed(0)}°</span>
+                <span className='weather-deg'>{cityDesc.temp}°</span>
             </div>
 
         </div>
--- src/context/CountryContext.js
+++ src/context/CountryContext.js
@@ -4,11 +4,23 @@ const CountryContext = createContext();
 
 export const CountryProvider = ({ children }) => {
 
+    const [dailyForecast, setDailyForecast] = useState([]);
     const [searchCity, setSearchCity] = useState('');
-    const [cityDesc, setCityDesc] = useState({
-        name: '',
-        temp: 0,
-    })
+    const [cityDesc, setCityDesc] = useState([
+        {
+            name: 'van',
+            datetime: '2022-09-19',
+            temp: '23.2',
+            icon: 's02d',
+        },
+        {
+            name: 'adana',
+            datetime: '2022-03-19',
+            temp: '12.1',
+            icon: 's03b',
+        }
+    ])
+
     const [submitVisible, setSubmitVisible] = useState(false);
 
     const data = {
@@ -17,7 +29,9 @@ export const CountryProvider = ({ children }) => {
         cityDesc,
         setCityDesc,
         submitVisible,
-        setSubmitVisible
+        setSubmitVisible,
+        dailyForecast,
+        setDailyForecast
     }
     return <CountryContext.Provider value={data}> {children} </CountryContext.Provider>
 }
